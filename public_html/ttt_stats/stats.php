2f7e15ef49bdcfdf15f1deef2c79ec3d0d7ba314
Nathan Hand <administrator@thehiddennation.com>
2013-02-04 19:21:53 +0000

    basic php script for outputting data

---
+++ public_html/ttt_stats/stats.php
@@ -0,0 +1,83 @@
+<?php
+/*------------------------\
+|        TTT STATS        |
+|	       Beta           |
+|=========================|
+|© 2013 SNGaming.org      |
+|	All Rights Reserved   |
+|=========================|
+| 	Website printout      |
+| 	   beta testing       |
+| 	   by Handy_man       |
+\------------------------*/
+
+$connect = mysql_connect("127.0.0.1", "handyman_ttt", "3213560921*+*");
+$db_select = mysql_select_db('handyman_ttt_stats');
+if (!connect) {
+
+	die('ERROR, Contact Handy_man immediately' . mysql_error());
+	
+}
+
+$var1 = mysql_query('SELECT * FROM `ttt_stats');
+$uniqueusers = mysql_num_rows($var1);
+
+$kills = mysql_query('SELECT SUM(kills) FROM ttt_stats');
+$killsarray = mysql_fetch_array($kills);
+$killstotal = array_sum($killsarray);
+
+$innocent = mysql_query('SELECT SUM(innocenttimes) FROM ttt_stats');
+$innocentarray = mysql_fetch_array($innocent);
+$innocenttotal = array_sum($innocentarray);
+
+$traitor = mysql_query('SELECT SUM(traitortimes) FROM ttt_stats');
+$traitorarray = mysql_fetch_array($traitor);
+$traitortotal = array_sum($traitorarray);
+
+$detective = mysql_query('SELECT SUM(detectivetimes) FROM ttt_stats');
+$detectivearray = mysql_fetch_array($detective);
+$detectivetotal = array_sum($detectivearray);
+
+$death = mysql_query('SELECT SUM(deaths) FROM ttt_stats');
+$deatharray = mysql_fetch_array($death);
+$deathtotal = array_sum($deatharray);
+
+$kills = mysql_query('SELECT SUM(kills) FROM ttt_stats');
+$killsarray = mysql_fetch_array($kills);
+$killstotal = array_sum($killsarray);
+
+$time = mysql_query('SELECT SUM(playtime) FROM ttt_stats');
+$timearray = mysql_fetch_array($time);
+$timetotal = array_sum($timearray);
+
+$seconds = $timetotal;
+			//start of math for hourse, minues and seconds
+				$hours = floor($seconds / (60 * 60));
+ 
+			// extract minutes
+				$divisor_for_minutes = $seconds % (60 * 60);
+				$minutes = floor($divisor_for_minutes / 60);
+ 
+			// extract the remaining seconds
+				$divisor_for_seconds = $divisor_for_minutes % 60;
+				$seconds = ceil($divisor_for_seconds);
+
+echo "Number of unique users : " . $uniqueusers . "</br>";
+echo "Total number of kills : " . $killstotal . "</br>";
+echo "Total number of innocents : " . $innocenttotal . "</br>";
+echo "Total number of detectives : " . $detectivetotal . "</br>";
+echo "Total number of traitors : " . $traitortotal . "</br>";
+echo "Total number of deaths : " . $deathtotal . "</br>";
+echo "Total number of kills: " . $killstotal . "</br>";
+
+echo "Total number of time played between all players is: " . $hours . " Hours " . $minutes . " Minutes and " . $seconds . " seconds. </br>";
+
+
+
+
+
+
+
+
+
+?>
