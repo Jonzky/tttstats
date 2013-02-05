ee75a8a932eaa506fdd5b1ef366e0f041b61ecf9
Jon Zakay <JonZakay@aol.com>
2013-02-05 19:23:48 +0000

    added SQL (working this time)

--- server_track.sql
+++ server_track.sql
@@ -36,13 +36,6 @@ CREATE TABLE IF NOT EXISTS `server_track` (
   PRIMARY KEY (`hostip`(20))
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `server_track`
---
-
-INSERT INTO `server_track` (`hostip`, `hostname`, `maxplayers`, `map`, `players`, `lastupdate`) VALUES
-
-
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
