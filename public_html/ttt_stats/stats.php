a6e050541fc64b03aee2c0dfd8a1b3957f127feb
Nathan Hand <administrator@thehiddennation.com>
2013-02-06 14:37:15 +0000

    moved sql into a config file, included in stats

--- public_html/ttt_stats/stats.php
+++ public_html/ttt_stats/stats.php
@@ -10,19 +10,7 @@
 | 	   beta testing       |
 | 	   by Handy_man       |
 \------------------------*/
-//include("/includes/header.php");
-
-/*SQL connection/ configuration goes here */
-
-$connect = mysql_connect("127.0.0.1", "handyman_ttt", "3213560921*+*");
-$db_select = mysql_select_db('handyman_ttt_stats');
-if (!connect) {
-
-	die('ERROR, Contact Handy_man immediately' . mysql_error());
-	
-}
-
-/*SQL connection/ configuration end here */
+include("./includes/header.php");
 
 /*Stats SQL queries all go here */
 
