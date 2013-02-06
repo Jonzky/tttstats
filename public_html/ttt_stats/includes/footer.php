38ff8ff4c4617bbb484877593592c97200380255
Nathan Hand <administrator@thehiddennation.com>
2013-02-06 13:40:10 +0000

    setting up "pretty" stuff still

--- public_html/ttt_stats/includes/footer.php
+++ public_html/ttt_stats/includes/footer.php
@@ -1,7 +1,10 @@
-    </div><!-- end:div#main -->
-    <div id="footer" class="container">
-        © 2013 SNGaming.org all rights reserved.
-    </div>
-  </div>
-  </body>
+				</div>
+				<div class="clearfooter"></div>
+				<div id="content_box_bottom">
+				<p id="lastmodified"> Copyright 2012 Nathan Hand (nah14@aber.ac.uk)</p><p>
+    <a href="http://validator.w3.org/check?uri=referer"><img
+      src="http://www.w3.org/Icons/valid-xhtml10" alt="Valid XHTML 1.0 Transitional" height="31" width="88" /></a>
+  </p>
+				</div>
+		</body>
 </html>
--- public_html/ttt_stats/includes/header.php
+++ public_html/ttt_stats/includes/header.php
@@ -1,22 +1,37 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Monster Mash</title>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-    <meta name="description" content="TTT Stats" />
-    <meta name="keywords" content="TTT, Trouble in terrorist town, stats, gaming, games, sngaming, sky-netgaming, skynet, gmod, garry's mod, Glua, lua, steam" />
-    <meta name="author" content="Handy_man" />
-    <link rel="stylesheet" href="../static/normalize.css" type="text/css" />
-    <link href='http://fonts.googleapis.com/css?family=Finger+Paint' rel='stylesheet' type='text/css'>
-    <link rel="stylesheet" href="../static/style.css" type="text/css" />
-  </head>
-  <body>
-  <div id="page">
-    <div id="header" class="container">
-        <h1>SNGaming.org TTT Stats</h1>
-        <ul>
-            <li><a href="http://www.sngaming.org">Home</a></li>
-        </ul>
-    </div>
-    <div id="main" class="container">
-<!-- end:includes/header.jsp -->
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+	<meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+		<link href="css/main.css" media="screen" rel="stylesheet" type="text/css" />
+		<?php
+		echo "<script type='text/javascript' src='scripts/script.js'></script>";
+		?>
+		<title>nah14@aber.ac.uk</title>
+	</head>
+			<body>
+				<div id="page">
+
+				<div id="logo">
+				<img src="images/game-store-logo-1.png" alt="The Hidden Game Store"/>
+				</div>
+						<div id='login'>
+					<?php 
+						//If we've got a username welcome them and offer logout, else offer login.
+						if (isset($_SESSION['username'])){
+							echo "Welcome, " . $_SESSION['username'] . ". " . "<a href='logout.php'>logout</a>";
+							}
+						else{
+							echo "Welcome, would you like to " . "<a href='index.php'>login?</a>";
+							}
+							?>
+					</div>
+					<div id="navigation">
+					<a href="index.php">Home</a>	<a href="about.php">About</a>	
+					<?php 
+					//because we re-direct them if they're not logged in, lets stop them from seeing basket and shop.
+					if (isset($_SESSION['username'])){
+							echo "<a href='basket.php'>Basket</a> ";
+							echo "<a href='shop.php'>Shop</a>";
+							}
+							?>
+					</div>
