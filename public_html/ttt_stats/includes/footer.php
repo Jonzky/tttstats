4105ccb449af70401067dd584bb1d5d2648ddb8b
Nathan Hand <administrator@thehiddennation.com>
2013-02-06 14:06:29 +0000

    footer updated, we don't care about validation anymore!

--- public_html/ttt_stats/includes/footer.php
+++ public_html/ttt_stats/includes/footer.php
@@ -15,9 +15,7 @@
 				</div>
 				<div class="clearfooter"></div>
 				<div id="content_box_bottom">
-				<p id="lastmodified"> Copyright 2012 Nathan Hand (nah14@aber.ac.uk)</p><p>
-    <a href="http://validator.w3.org/check?uri=referer"><img
-      src="http://www.w3.org/Icons/valid-xhtml10" alt="Valid XHTML 1.0 Transitional" height="31" width="88" /></a>
+				<p id="lastmodified"> Copyright 2013 SNGaming.org</p><p>
   </p>
 				</div>
 		</body>
