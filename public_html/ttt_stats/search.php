0aaa5d8505d046b6c39cbf8117e64d7b1284fb35
Nathan Hand <administrator@thehiddennation.com>
2013-02-21 18:59:47 +0000

    Who wanted a pie chart? Handy_man wanted a pie chart

--- public_html/ttt_stats/search.php
+++ public_html/ttt_stats/search.php
@@ -137,7 +137,44 @@ echo "<td>" . $playerFirstjoined . "</td>";
 echo "<td> <a href=" . $sb_search_string . "/>" . $bannedTotal . "</td>";
 echo "</tr>";
 echo "</table>";
-echo "</div>";
 
+?>
+  <script type="text/javascript" src="https://www.google.com/jsapi"></script>
+    <script type="text/javascript">
+
+      // Load the Visualization API and the piechart package.
+      google.load('visualization', '1.0', {'packages':['corechart']});
+
+      // Set a callback to run when the Google Visualization API is loaded.
+      google.setOnLoadCallback(drawChart);
+
+      // Callback that creates and populates a data table,
+      // instantiates the pie chart, passes in the data and
+      // draws it.
+      function drawChart() {
+
+        // Create the data table.
+        var data = new google.visualization.DataTable();
+        data.addColumn('string', 'Topping');
+        data.addColumn('number', 'Slices');
+        data.addRows([
+          ['Traitor', <?echo$playerInnocenttimes;?>],
+          ['Detective', <?echo$playerDetectivetimes;?>],
+          ['Innocent', <?echo$playerTraitortimes;?>]
+        ]);
+
+        // Set chart options
+        var options = {'title':'Traitor, Detective, Innocent Times.',
+                       'width':400,
+                       'height':300};
+
+        // Instantiate and draw our chart, passing in some options.
+        var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
+        chart.draw(data, options);
+      }
+    </script>
+<div id="chart_div"></div>
+</div>
+<?
 include("./includes/footer.php");
 ?>
