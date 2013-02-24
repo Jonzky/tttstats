73d3155c36754f15663341095144a4d7cf8bb433
Nathan Hand <administrator@thehiddennation.com>
2013-02-24 03:42:23 +0000

    Wrote up some minor information about the about page... ask me/ make changes as you like.

--- public_html/ttt_stats/about.php
+++ public_html/ttt_stats/about.php
@@ -13,12 +13,23 @@
 
 include("./includes/header.php");
 ?>
-	<div id="primary_content">
+	<div id="primary_content_about">
+	<p><img src="./static/images/ttt_logo.png" alt="Trouble in terrorist town!" /></p>
+	<p>Welcome to the About page for the Trouble in Terrorist town Stat tracker!, The TTT Stat tracker is currently in development and not ready for any kind of final release.
+	Although it's not ready you can still freely see and download the code on the <a href="https://github.com/Jonzky/tttstats">GitHub page</a>.</p>
 	
-	<?php
-
-?>
-
+	<p>The project currently only has 2 developers working in their spare time to make this addon to the popular Garry's mod gamemode Trouble in terrorist town, If you want to learn more about Trouble in terrorist town please see these links. <a href="http://ttt.badking.net/">TTT Homepage</a> <a href="http://www.moddb.com/mods/trouble-in-terrorist-town">MOD DB</a><p>
+	
+	<p>The Stat tracker currently tracks the following stats for every player in the server: Steamid, Nickname, Playtime, Rounds played, number of times Innocent, Detective & traitor, Deaths, Kills, highest score (per map/ session), First time seen on the server.</p>
+	
+	<p>There are a few other stats that are being planned to be tracked but are not currently in implementation, these include: Last time seen in the server & Points (custom point system to be determined based on kills, deaths, innocent vs. innocent kills etc becoming a large persistant number)</p>
+	
+	<p>There are also plans to implement in the search bar the number of bans a user has incurred on the server, this interfaces with sourcebans as it's the ban method used by <a href="http://www.sngaming.org">SNGaming.org</a> and will be included in a release as an optional feature</p>
+	
+	<h3>The Developers!</h3>
+	
+	<h4>Jonzky</h4> Jonzky is responsible for all of the code found in the garry's mod addon or the lua side of the project, also credited with the initial database design. <a href="http://steamcommunity.com/id/jonzky1/">Steam</a>
+	<h4>Handy_man</h4> Handy_man is responsible for all of the PHP code which drives the output of the project's stats on the web, also credited with upkeeping/ updating the initial database design for more fields etc. <a href="http://steamcommunity.com/id/Handy_man/">Steam</a>
 	
 							
 	</div>
