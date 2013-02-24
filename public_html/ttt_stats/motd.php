798ef9d4686b0c0cb9c68da3500d0dd0e064fea5
Nathan Hand <administrator@thehiddennation.com>
2013-02-24 20:47:17 +0000

    Added pie chart to the MOTD, not current displaying it, might look bad in small game sizes

--- public_html/ttt_stats/motd.php
+++ public_html/ttt_stats/motd.php
@@ -83,6 +83,40 @@ $seconds = $playerPlaytime;
 
 
 ?>
+<script type="text/javascript" src="https://www.google.com/jsapi"></script>
+    <script type="text/javascript">
+
+      // Load the Visualization API and the piechart package.
+      google.load('visualization', '1.0', {'packages':['corechart']});
+
+      // Set a callback to run when the Google Visualization API is loaded.
+      google.setOnLoadCallback(drawChart);
+
+      // Callback that creates and populates a data table,
+      // instantiates the pie chart, passes in the data and
+      // draws it.
+      function drawChart() {
+
+        // Create the data table.
+        var data = new google.visualization.DataTable();
+        data.addColumn('string', 'Topping');
+        data.addColumn('number', 'Slices');
+        data.addRows([
+          ['Innocent', <?echo$playerInnocenttimes;?>],
+          ['Detective', <?echo$playerDetectivetimes;?>],
+          ['Traitor', <?echo$playerTraitortimes;?>]
+        ]);
+
+        // Set chart options
+        var options = {'title':'Traitor, Detective, Innocent Times.',
+                       'width':400,
+                       'height':300};
+
+        // Instantiate and draw our chart, passing in some options.
+        var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
+        chart.draw(data, options);
+      }
+    </script>
 <div id="primary_content">
 <h3>TTT Stat tracker! Here are your TTT stats for Sngaming.org's TTT servers.</h3>
 </br>
@@ -123,7 +157,11 @@ echo "<td>" . $playerFirstjoined . "</td>";
 echo "<td>" . $bannedTotal . "</td>";
 echo "</tr>";
 echo "</table>";
+//echo "<div id='chart_div'></div>";
 echo "</div>";
+?>
 
+
+<?
 include("./includes/footer.php");
 ?>
