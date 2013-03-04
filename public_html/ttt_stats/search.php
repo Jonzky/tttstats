c6558062ae451ec05fe8ec1ac3e8f7f3edabde32
Nathan Hand <administrator@thehiddennation.com>
2013-03-04 10:40:56 +0000

    am i retarded? i think i am.

--- public_html/ttt_stats/search.php
+++ public_html/ttt_stats/search.php
@@ -101,7 +101,7 @@ $playerMaxfrags = $playerarray['maxfrags'];
 $playerFirstjoined = $playerarray['first_joined'];
 $playerLastSeen = $playerarray['last_seen'];
 $sb_search_string = "http://bans.sngaming.org/index.php?p=banlist&searchText=" . $playerSteamid . "&Submit=";
-if ($playerKills or $playerDeaths != 0){
+if ($playerKills and $playerDeaths != 0){
 $playerKDRTrun = $playerKills / $playerDeaths;
 $playerKDR = round($playerKDRTrun, 2); //rounding to numbers such as 0.12 rather then 0.1259848797 etc. We don't need that many decimal points in our output, no one cares for that level of accuracy. 
 }
