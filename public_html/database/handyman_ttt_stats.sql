805da65b4d91e8a5c0406dfb9af671e30cd379de
Nathan Hand <administrator@thehiddennation.com>
2013-03-14 15:59:20 +0000

    adding google adsense code, removed old and useless sql files added current DB structure

---
+++ public_html/database/handyman_ttt_stats.sql
@@ -0,0 +1,102 @@
+-- phpMyAdmin SQL Dump
+-- version 3.4.11.1
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Mar 14, 2013 at 10:57 AM
+-- Server version: 5.5.23
+-- PHP Version: 5.2.17
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `handyman_ttt_stats`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin_users`
+--
+
+CREATE TABLE IF NOT EXISTS `admin_users` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `email` text COLLATE utf8_unicode_ci NOT NULL,
+  `user` text COLLATE utf8_unicode_ci NOT NULL,
+  `pass` text COLLATE utf8_unicode_ci NOT NULL,
+  `last_login` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
+  `last_ip` text COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`ID`),
+  UNIQUE KEY `unique usernames` (`user`(100))
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=26 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `server_track`
+--
+
+CREATE TABLE IF NOT EXISTS `server_track` (
+  `hostip` text NOT NULL,
+  `hostname` text NOT NULL,
+  `maxplayers` int(10) NOT NULL,
+  `map` text NOT NULL,
+  `players` int(11) NOT NULL,
+  `lastupdate` int(11) NOT NULL,
+  PRIMARY KEY (`hostip`(20))
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ttt_report`
+--
+
+CREATE TABLE IF NOT EXISTS `ttt_report` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `steamid` text COLLATE utf8_unicode_ci NOT NULL,
+  `nickname` text COLLATE utf8_unicode_ci NOT NULL,
+  `lKarma` int(5) NOT NULL,
+  `opKills` int(5) NOT NULL,
+  `message` text COLLATE utf8_unicode_ci NOT NULL,
+  `repID` text COLLATE utf8_unicode_ci NOT NULL,
+  `repNick` text COLLATE utf8_unicode_ci NOT NULL,
+  `report_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=69 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ttt_stats`
+--
+
+CREATE TABLE IF NOT EXISTS `ttt_stats` (
+  `steamid` text NOT NULL,
+  `nickname` text NOT NULL,
+  `playtime` bigint(20) NOT NULL DEFAULT '1',
+  `roundsplayed` int(10) NOT NULL,
+  `innocenttimes` int(10) NOT NULL,
+  `detectivetimes` int(10) NOT NULL,
+  `traitortimes` int(10) NOT NULL,
+  `deaths` int(10) NOT NULL,
+  `kills` int(10) NOT NULL,
+  `points` int(11) NOT NULL,
+  `maxfrags` int(10) NOT NULL,
+  `headshots` int(11) NOT NULL,
+  `first_joined` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `last_seen` text NOT NULL,
+  `isadmin` int(11) NOT NULL,
+  PRIMARY KEY (`steamid`(15))
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- public_html/ttt_stats/includes/advert_foot.php
+++ public_html/ttt_stats/includes/advert_foot.php
@@ -15,7 +15,17 @@
 				
 				</div>
 				<div id="advert_foot">
-				Place Advert Code here!
+				<script type="text/javascript"><!--
+				google_ad_client = "ca-pub-6665064114968712";
+				/* footer_ad */
+				google_ad_slot = "5706410197";
+				google_ad_width = 728;
+				google_ad_height = 90;
+				//-->
+				</script>
+<script type="text/javascript"
+src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+</script>
 				</div>
 		</body>
 </html>
