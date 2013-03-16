3589fc86f607ce3abfae6af5dcff0b85dbacf62c
Nathan Hand <administrator@thehiddennation.com>
2013-03-16 06:33:44 +0000

    We're calling ourselves when we remove someone, so lets order things properly.

--- public_html/ttt_stats/control/control-reports.php
+++ public_html/ttt_stats/control/control-reports.php
@@ -13,8 +13,20 @@
 include("./includes/header.php");
 include("./includes/config.php");	
 
+
+
+$removeID=$_POST['reportID'];
+$removedName=$_POST['reportName'];
+
+if(isset($removeID)){
+$delete = mysql_query("DELETE FROM `ttt_report` WHERE `id` = $removeID");
 $allreports = mysql_query("SELECT * FROM `ttt_report` ORDER BY `ttt_report`.`id` DESC");
 $multiResult = mysql_num_rows($allreports);
+}
+else{
+$allreports = mysql_query("SELECT * FROM `ttt_report` ORDER BY `ttt_report`.`id` DESC");
+$multiResult = mysql_num_rows($allreports);
+}
 
 
 ?>
@@ -22,9 +34,14 @@ $multiResult = mysql_num_rows($allreports);
 
 <h4><? echo $multiResult . " report(s) currently waiting your attention.";?></h4>
 
+<?PHP
+if (isset($removeID)){
+	echo "<script type='text/javascript' language='JavaScript'> alert('You successfully removed the report against " . $removedName . "')</script>";
+}
+?>
+
 <table border ="1">
 						<tr>
-						<th>ReportID</th>
 						<th>SteamID</th>
 						<th>Nickname</th>
 						<th>Karma</th>
@@ -33,12 +50,14 @@ $multiResult = mysql_num_rows($allreports);
 						<th>reporterID</th>
 						<th>reporterNickname</th>
 						<th>Report Time</th>
+						<th>Remove?</th>
 						</tr>
 
 <?
 
 
 while($reportArray = mysql_fetch_array( $allreports )) {
+echo "<form action='".$_SERVER['PHP_SELF']."' method='post'>";
 $reportID = $reportArray['id'];
 $reportSteamid = $reportArray['steamid'];
 $reportNick = $reportArray['nickname'];
@@ -50,7 +69,6 @@ $reporterNick = $reportArray['repNick'];
 $reportTime = $reportArray['report_time'];
 
 echo "<tr>";
-echo "<td>" . $reportID . "</td>";
 echo "<td><a href='http://bans.sngaming.org/index.php?p=banlist&searchText=" . $reportSteamid . "&Submit='>" . $reportSteamid . "</a></td>";
 echo "<td>" . $reportNick . "</td>";
 echo "<td>" . $reportKarma . "</td>";
@@ -59,8 +77,9 @@ echo "<td>" . $reportMessage . "</td>";
 echo "<td>" . $reporterID . "</td>";
 echo "<td>" . $reporterNick . "</td>";
 echo "<td>" . $reportTime . "</td>";
+echo "<td> <input type='hidden' name='reportID' id='reportID' value='" . $reportID . "'> <input type='hidden' name='reportName' id='reportName' value='" . $reportNick . "'> <button class='button' type='submit'>Remove</button></td>";
 echo "</tr>";
-
+echo "</form>";
 	
 } 
 
