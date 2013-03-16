7d48540ca0e03dc5d5997a22116e491bac3671d2
Nathan Hand <administrator@thehiddennation.com>
2013-03-16 22:26:36 +0000

    Working add-admin in the back-end with hash

--- public_html/ttt_stats/control/add-admin-process.php
+++ public_html/ttt_stats/control/add-admin-process.php
@@ -11,6 +11,7 @@ $mypassword=$_POST['password'];
 $myemail=$_POST['e-mail'];
 $mySteam=$_POST['steamID'];
 $myAdminType=$_POST['admin']; //admin or superAdmin please :D 
+$myHash = md5( rand(0,1000) );
 
 // To protect MySQL injection (more detail about MySQL injection)
 $myusername = mysql_real_escape_string($myusername);
@@ -42,7 +43,8 @@ $_SESSION['failedReg'] = true;
 header('Location: http://' . $_SERVER['HTTP_HOST'] . '/ttt_stats/control/add-admin.php');
 }
 else {
-$check = mysql_query("INSERT INTO `handyman_ttt_stats`.`admin_users` (`ID`, `email`, `user`, `pass`, `last_login`, `last_ip`, 'steamID', 'isadmin') VALUES (NULL, '$myemail', '$myusername', MD5('$mypassword'), '0000-00-00 00:00:00', '127.0.0.1', '$mySteam', '$myAdminType')");
+//$check = mysql_query("INSERT INTO `handyman_ttt_stats`.`admin_users` (`ID`, `email`, `user`, `pass`, `last_login`, `last_ip`, 'steamID', 'isadmin') VALUES (NULL, '$myemail', '$myusername', MD5('$mypassword'), '0000-00-00 00:00:00', '127.0.0.1', '$mySteam', '$myAdminType')");
+$check = mysql_query("INSERT INTO `handyman_ttt_stats`.`admin_users` (`ID`, `email`, `user`, `pass`, `last_login`, `last_ip`, `steamID`, `isadmin`, `hash`, `active`) VALUES (NULL, '$myemail', '$myusername', MD5('$mypassword'), '0000-00-00 00:00:00', '127.0.0.0', '$mySteam', '$myAdminType', '$myHash', '0')");
 header('Location: http://' . $_SERVER['HTTP_HOST'] . '/ttt_stats/login.php');
 }
 
