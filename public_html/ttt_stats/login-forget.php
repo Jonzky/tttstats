da978df04e6ea42dbdeba7940c6d03da80deeab2
Nathan Hand <administrator@thehiddennation.com>
2013-03-16 12:35:54 +0000

    what if we forget our login username? login username reminder script! (basic)

---
+++ public_html/ttt_stats/login-forget.php
@@ -0,0 +1,74 @@
+<?php
+/*------------------------\
+|        TTT STATS        |
+|          Beta           |
+|=========================|
+|© 2013 SNGaming.org      |
+|   All Rights Reserved   |
+|=========================|
+|   Website printout      |
+|      beta testing       |
+|      by Handy_man       |
+\------------------------*/				
+
+include("./includes/header.php");
+include("./includes/config.php");	
+
+if (isset($_POST['email'])){
+
+$myemail=$_POST['email'];
+$myemail = mysql_real_escape_string($myemail);
+
+$check = mysql_query("SELECT * FROM admin_users WHERE email='$myemail'");
+$users = mysql_num_rows($check);
+
+
+if ($users == 1){
+$to = $_POST['email'];
+
+while($playerarray = mysql_fetch_array( $check )) {
+$theusername = $playerarray['user'];
+}
+$subject = '[TTT STATS] Username reminder';
+
+$message = "Automated message from www.thehiddennation.com
+
+Your username was requested to be re-sent to your e-mail address listed for the TTT Stat tracker!
+
+Your username from TTT stats is as follows:". $theusername . "
+
+Thanks,
+
+[TTT STAT TACKER] copyright www.thehiddennation.com";
+
+
+
+$headers = 'From: no-reply@thehiddennation.com' . "\r\n" .
+    'Reply-To: no-reply@thehiddennation.com' . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+
+mail($to, $subject, $message, $headers);
+echo "<p class ='noexist'>Success! e-mail sent to " . $to . "</p>";
+}
+else{
+echo "<p class ='noexist'>That E-mail address doesn't exist! are you sure you're <a href='./register.php'>registered</a>?</p>";
+}
+
+}
+else{
+
+}
+echo "<form action='".$_SERVER['PHP_SELF']."' method='post'>";
+
+?>
+
+<p class="center">
+<strong>Forgotten your username?</strong><br/>
+<strong>Please enter your e-mail address</strong>
+<input name="email" type="text" id="email" required>
+<button class='button' type='submit'>send</button>
+</form>
+
+<p class="center">Have no login? Why not try and <a href="register.php">register</a>?</p>
+</p>
+<?PHP include("./includes/footer.php");?>
