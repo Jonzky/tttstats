64c1042440cd4e51b997555e3c91080e199fcb68
Nathan Hand <administrator@thehiddennation.com>
2013-03-17 12:14:11 +0000

    if they're not verified lets allow them to recieve a new verification link (with a new hash)

--- public_html/ttt_stats/verify.php
+++ public_html/ttt_stats/verify.php
@@ -14,6 +14,51 @@
 include("./includes/header.php");
 include("./includes/config.php");
 
+if(isset($_POST['myemail'])){
+$verifyEmail = $_POST['myemail'];
+$checkemail = mysql_query("SELECT * FROM admin_users WHERE email='$verifyEmail' AND active = 0");
+$usersemail = mysql_num_rows($checkemail);
+
+$checkemailactive = mysql_query("SELECT * FROM admin_users WHERE email='$verifyEmail' AND active = 1");
+$usersemailactive = mysql_num_rows($checkemailactive);
+
+if($usersemail==1){ //great they exist, lets send that e-mail with a new hash.
+$myHash = md5( rand(0,1000) );
+$newHash = mysql_query("UPDATE `admin_users` SET `hash`='$myHash' WHERE `email` = '$verifyEmail'");
+
+$to = $verifyEmail;
+$subject = '[TTT STATS] Account Verification';
+
+$message = "Automated message " . $_SERVER['HTTP_HOST'] . "/ttt_stats 
+
+Hello, " . $myusername . ". 
+
+Please Verify your account by clicking the following link : http://" . $_SERVER['HTTP_HOST'] . "/ttt_stats/verify.php?email=" . $verifyEmail . "&hash=" . $myHash . "
+
+Thanks,
+
+[TTT STAT TACKER] - " . $_SERVER['HTTP_HOST'] . "/ttt_stats";
+
+
+
+$headers = 'From: no-reply@' . $_SERVER['HTTP_HOST'] . '' . "\r\n" .
+    'Reply-To: no-reply@' . $_SERVER['HTTP_HOST'] . '' . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+
+mail($to, $subject, $message, $headers);
+
+
+echo "<p class ='noexist'>Your new verification E-mail has been sent.</p>";
+}
+else if ($usersemailactive==1){
+echo "<p class ='noexist'>Your account is already active!</p>";
+}
+else{
+echo "<p class ='noexist'>That account doesn't exist! are you sure you're <a href='./register.php'>registered</a>?</p>";
+}
+
+}
+
 if(isset($_GET['email']) && !empty($_GET['email']) AND isset($_GET['hash']) && !empty($_GET['hash'])){  
 $adminEmail = $_GET['email'];
 $adminHash = $_GET['hash'];
@@ -33,8 +78,17 @@ echo "<p class ='noexist'>Your account is already active!</p>";
 
 
 }
-else{
-echo "Invalid!";
+else{ //Lets re-send our verification e-mail if the first one failed for unknown reasons.
+echo "<form action='".$_SERVER['PHP_SELF']."' method='post'>";
+echo "<p class='center'>";
+echo "<strong>Re-send Verification E-mail</strong><br/>";
+echo "</p>";
+echo "<span class='formData'>E-mail :</span>";
+echo "<input name='myemail' type='text' id='myemail' placeholder='E-mail' required>";
+echo "<br/>";
+echo "<p class='center'>";
+echo "<button class='button' type='submit'>re-send</button></form>";
+echo "</p>";
 }
 ?>
 
