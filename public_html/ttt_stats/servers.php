12f941d03b48d7f770c46e99928dc414d78ccc6e
Nathan Hand <administrator@thehiddennation.com>
2013-04-01 21:58:38 +0100

    very basic server page starting to take place, will add more to it at a later date.

---
+++ public_html/ttt_stats/servers.php
@@ -0,0 +1,35 @@
+<?php
+/*------------------------\
+|        TTT STATS        |
+|	       Beta       |
+|=========================|
+|� 2013 SNGaming.org      
+|  All Rights Reserved    |
+|=========================|
+| 	Website printout  |
+| 	   beta testing   |
+| 	   by Handy_man   |
+\------------------------*/
+include("./includes/header.php");
+include("./includes/config.php");	
+/*Setup variables for SQL statements */
+
+/*Stats SQL queries end here */
+
+/*Maths for any functions go here */
+				
+/*Maths for functions end here */
+
+/*print statements */
+
+echo "<div id='primary_content'>";
+
+
+echo"<h3>Servers!</h3><table border ='1'><tr><th>Hostname</th><th>Players</th><th>Map</th></tr>";
+
+
+echo "</table>";
+}
+echo "</div>";
+include("./includes/footer.php");
+?>
