52c511c7ac8ebb5e69e8e764db6886d76d7eb0e9
Nathan Hand <administrator@thehiddennation.com>
2013-04-02 18:52:11 +0100

    MOAR CHANGES! got rid of the primary content we've got nothing to read there and added auto width

--- public_html/ttt_stats/servers.php
+++ public_html/ttt_stats/servers.php
@@ -24,10 +24,7 @@ $server_list = mysql_query("SELECT * FROM `server_track`");
 
 /*print statements */
 
-echo "<div id='primary_content'>";
-
-
-echo"<h3>This is a live list of the game servers running the server tracker module!</h3><table border ='1' class='servertable'><tr><th>Hostname</th><th>Players</th><th>Current Map</th><th>Connect to server</th></tr>";
+echo"<p class='noexist'>This is a live list of the game servers running the server tracker module!</p><table border ='1' class='servertable'><tr><th>Hostname</th><th>Players</th><th>Current Map</th><th>Connect to server</th></tr>";
 
 while($row2 = mysql_fetch_array( $server_list )) {
 		
@@ -42,6 +39,5 @@ while($row2 = mysql_fetch_array( $server_list )) {
 
 
 echo "</table>";
-echo "</div>";
 include("./includes/footer.php");
 ?>
