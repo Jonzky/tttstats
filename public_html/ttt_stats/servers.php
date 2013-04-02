588365a2fcddc7ffe38c91a10d547440d373c557
Nathan Hand <administrator@thehiddennation.com>
2013-04-02 17:00:28 +0100

    basic server page up and running, shows server details and connect button.

--- public_html/ttt_stats/servers.php
+++ public_html/ttt_stats/servers.php
@@ -14,6 +14,8 @@ include("./includes/header.php");
 include("./includes/config.php");	
 /*Setup variables for SQL statements */
 
+$server_list = mysql_query("SELECT * FROM `server_track`");
+
 /*Stats SQL queries end here */
 
 /*Maths for any functions go here */
@@ -25,11 +27,21 @@ include("./includes/config.php");
 echo "<div id='primary_content'>";
 
 
-echo"<h3>Servers!</h3><table border ='1'><tr><th>Hostname</th><th>Players</th><th>Map</th></tr>";
+echo"<h3>This is a live list of the game servers running the server tracker module!</h3><table border ='1'><tr><th>Hostname</th><th>Players</th><th>Current Map</th><th>Connect to server</th></tr>";
+
+while($row2 = mysql_fetch_array( $server_list )) {
+		
+	echo "<tr><td>"; 
+	echo $row2['hostname']; 
+	echo "</td><td> " . $row2['players'] . "/ " . $row2['maxplayers'] . "</td>";
+	echo "<td> " . $row2['map'] . "</td>";
+	echo "<td> <form action='steam://connect/" . $row2['hostip'] . "'><button class='button' type='submit'>Connect</button></form> </td>";
+	echo "</tr>";
+	
+} 
 
 
 echo "</table>";
-}
 echo "</div>";
 include("./includes/footer.php");
 ?>
