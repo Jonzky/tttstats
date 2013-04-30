e49b790a7b70cd70ce4eb639cc01d8e18214a1c0
Nathan Hand <administrator@thehiddennation.com>
2013-04-30 11:54:49 +0100

    slightly sexier footer stuff.

--- public_html/ttt_stats/includes/footer.php
+++ public_html/ttt_stats/includes/footer.php
@@ -14,14 +14,18 @@ $input = array("testing1", "testing2", "Testing3", "testing4", "testing5");
 $rand_keys = array_rand($input, 2);
 $quotes = $input[$rand_keys[0]] . "\n";
 ?>
-				
+				<div class="clearfooter">
 				</div>
-				<div class="clearfooter"></div>
-				<div id="content_box_bottom">
+				</div>
+			
+			
+				<div class="siteFooter">
+				<div class="center">
 				
 				<a href="mailto:administrator@thehiddennation.com">Report a Problem</a><br/>
 				<?PHP
 				//echo $quotes;?>
+				</div>
 				</div>
 		</body>
 </html>
