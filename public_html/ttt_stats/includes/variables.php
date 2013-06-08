a76269ddb18f4bab450f4d14c20bda7f12272a2b
Nathan Hand <administrator@thehiddennation.com>
2013-06-08 17:14:56 +0100

    We're going to have a number of global variables to store information.

---
+++ public_html/ttt_stats/includes/variables.php
@@ -0,0 +1,35 @@
+<?php
+/*------------------------\
+|        TTT STATS        |
+|	       Beta           |
+|=========================|
+|© 2013 SNGaming.org      |
+|	All Rights Reserved   |
+|=========================|
+| 	Website printout      |
+| 	   beta testing       |
+| 	   by Handy_man       |
+\------------------------*/
+
+$sb_enabled = true;
+
+$base_address = "www.sngaming.org"
+
+$reports_enabled = true;
+
+$servers_enabled = true;
+
+$register_enabled = true;
+
+$facebook_link = "sngaming2013";
+$twitter_link = "skynet_gaming";
+$youtube_link = "SkyNetNation";
+$steam_link = "sky-netgaming";
+$twitch_link = "skynetgaming";
+
+$logo_path = "/static/images/skynet_logo.png";
+
+$badge_ref = "www.thehiddennation.com/ttt_stats";
+
+
+?>
