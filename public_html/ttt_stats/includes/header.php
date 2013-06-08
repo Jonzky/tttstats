46ef62744536f0dedaadf7e10bdb6687ab8fa53e
Nathan Hand <administrator@thehiddennation.com>
2013-06-08 17:15:33 +0100

    derp

--- public_html/ttt_stats/includes/header.php
+++ public_html/ttt_stats/includes/header.php
@@ -11,7 +11,7 @@
 | 	   by Handy_man       |
 \------------------------*/		
 require("./includes/session_start.php");	
-include_once("./includs/variables.php");
+include_once("./includes/variables.php");
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
