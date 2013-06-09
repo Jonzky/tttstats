31aa0af2cf18c989a77b05aad55aed8127e517f9
Nathan Hand <administrator@thehiddennation.com>
2013-06-09 02:28:04 +0100

    downloaded automated variabled.php code to local/git and deleted test file.

--- public_html/ttt_stats/includes/variables.php
+++ public_html/ttt_stats/includes/variables.php
@@ -1,17 +1,7 @@
-<?php
-/*------------------------\
-|        TTT STATS        |
-|	       Beta           |
-|=========================|
-|© 2013 SNGaming.org      |
-|	All Rights Reserved   |
-|=========================|
-| 	Website printout      |
-| 	   beta testing       |
-| 	   by Handy_man       |
-\------------------------*/
+<?php 
 
-$sb_enabled = false;
+$sb_enabled = true;
+
 $sb_search_build = "http://bans.sngaming.org/index.php?p=banlist&searchText=";
 
 $base_address = "http://www.sngaming.org";
@@ -23,17 +13,19 @@ $servers_enabled = true;
 $register_enabled = true;
 
 $badge_enabled = true;
+
 $badge_ref = "www.thehiddennation.com/ttt_stats";
 
 $facebook_link = "sngaming2013";
+
 $twitter_link = "skynet_gaming";
+
 $youtube_link = "SkyNetNation";
+
 $steam_link = "sky-netgaming";
+
 $twitch_link = "skynetgaming";
 
 $logo_path = "./static/images/skynet_logo.png";
-
-
-
 
 ?>
