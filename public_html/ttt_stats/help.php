d3aaf5336072ff9365f4f91e743acf7862442eea
Nathan Hand <administrator@thehiddennation.com>
2013-06-12 15:59:03 +0100

    Help made to look better

--- public_html/ttt_stats/help.php
+++ public_html/ttt_stats/help.php
@@ -18,10 +18,13 @@ include("./includes/header.php");
 	
 	<h5>The web stuff!</h5>
 	
-	<p>Installation of the webstuff is very easy and straight forward, first of all you need to create a new user and database and give this user access to the newly created database. This user needs to have the normal permissions as well as create tables (because the creation of the SQL tables is automated/ achieved during setup).</p>
-	
-	<p>Once you have created the database user, remember that information! we'll need it later! next we simply goto yourwebsite/ttt_stats/install/index.php from there all you have to do is follow the instructions/ forms and everything should be taken care of.</p>
-	
+	<p><b>1.</b> Upload the folder "ttt_stats" from the web_upload folder to you webserver, keeping attention that i'm bad and for now it should stay in a folder called "ttt_stats".</p>
+	<p><b>2.</b> Create a new database user, give it the normal permissions as well as "create" giving it access to the new database you've setup.</p>
+	<p><b>3.</b> goto www.yourwebsitehere/ttt_stats/install/ and follow the forms, paying attention to the fact that you will need your database information.</p>	
+	<p><b>4.</b> Once installation has been complete check that you're able to log in with the account you created during installation (Check your e-mail for a verification from the installation.)</p>	
+	<p><b>5.</b> Once you've confirmed your ability to log into the control panel, delete the /install directory from your webserver.</p>	
+	<p><b>6.</b> If your game server is correctly connecting to this database and saving information you should start to see pages fill up with information.</p>	
+		
 	<h5>The Game server stuffs!</h5>
 
 	<p>Jonzky! You should come over here! and write stuff!</p>
