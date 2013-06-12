4981aa999ada63fee32d880d14872af08e583d2c
Nathan Hand <administrator@thehiddennation.com>
2013-06-12 18:51:20 +0100

    we're live on initial release, no big deal.

--- public_html/ttt_stats/index.php
+++ public_html/ttt_stats/index.php
@@ -17,5 +17,7 @@ include("./includes/header.php");
 							<p>Welcome to Handy_man's and Jonzky's Trouble in Terrorist Town Stat tracking website! This website is in Beta, feel free to check everything out and report any problems.</p>
 							<p>Here is an example of my latest attempt at a "badge" <img src="http://www.thehiddennation.com/ttt_stats/badge.php?i=STEAM_0:1:24438298&t=all"></img></p>
 							<p>The idea of the badge system allows users to dynamically generate a smallish image with all of their TTT stats from the SNGaming.org's servers</p>
+							</br>
+							<p>This has now been released! goto : <a href="http://facepunch.com/showthread.php?t=1277865">Facepunch</a> to find out more!
 						</div>
 				<?PHP include("./includes/footer.php");?>
