919aacc6b141554bc0b12815337a403c6c73deac
Nathan Hand <administrator@thehiddennation.com>
2013-06-18 12:44:57 +0100

    2 questions and answers i can generally take from some forum stuff

--- public_html/ttt_stats/faq.php
+++ public_html/ttt_stats/faq.php
@@ -16,7 +16,13 @@ include("./includes/header.php");
 	<div id="primary_content_about">
 	<h3>The FAQ</h3>
 	
-	<p>We actually don't have any asked questions yet... well this is embarrassing</p>
+	<p><b>Q:</b>How can i get the vbulliten side block as seen on sngaming.org's forum?</br>
+	<b>A:</b>Check your download, all the code for that sideblock is seen in the ttt_stats folder under the name vb_module_example.dat</p>
+	
+	
+	<p><b>Q:</b>I'm pretty sure that i've set everything up correctly, but i still don't see any stats on my website what could be wrong?</br>
+	<b>A:</b>Most website hosts limit remoteSQL interaction based on your IP address, not allowing users to connect to the SQL databse unless they're on the IP address whitelist. Make sure that your server is on this whitelist so that your gameserer can talk to your webSQL.</p>
+	
 	
 	</div>
 <?PHP include("./includes/footer.php");?>
